6a2fcf6db165f7a6d39e078a9b343701b1f07d60
patrick <patrick@helmsdeep.at>
2012-04-01 09:43:02 +0200

    start script direct from cmdline

--- moodlefetch.py
+++ moodlefetch.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #########################################################################
 # This program is free software: you can redistribute it and/or modify  #
 # it under the terms of the GNU General Public License as published by  #
@@ -13,7 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>. #
 #########################################################################
 
-__version__ = '0.1'
+__version__ = "0.1"
 
 import re
 import sys, os, ConfigParser
