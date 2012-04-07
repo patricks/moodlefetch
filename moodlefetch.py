aa5aa1858e43b41c47dc9f2decf3c84cd19a949f
Manuel Hofer <manuel@mnlhfr.at>
2012-04-07 05:42:00 +0200

    refactoring, object oriented, threading

--- moodlefetch.py
+++ moodlefetch.py
@@ -1,275 +1,172 @@
-#!/usr/bin/env python
-
-#########################################################################
-# This program is free software: you can redistribute it and/or modify  #
-# it under the terms of the GNU General Public License as published by  #
-# the Free Software Foundation, either version 3 of the License, or     #
-# (at your option) any later version.                                   #
-#                                                                       #
-# This program is distributed in the hope that it will be useful,       #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-# GNU General Public License for more details.                          #
-#                                                                       #
-# You should have received a copy of the GNU General Public License     #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>. #
-#########################################################################
-
-__version__ = "0.1"
-
 import re
 import sys, os, ConfigParser
 import urllib
 import urllib2
 import getpass
 import cookielib
+import new
+import threading, time
+import logging
 
-#
-# DEFAULT CONFIGURATION PATH GOES HERE
-DEFAULTCONFIG = "~/.moodlefetch.cfg"
+logger = logging.getLogger('moodlefetch')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('/dev/stdout')
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
 
-#since the default urllib2 HTTPRedirectHandler leaves us no option to disable auto-redirects
-#we have to implement our own Handler in order to get the final urls and original filenames from moodle
 class MyHTTPRedirectHandler(urllib2.HTTPRedirectHandler):
   def http_error_303(self, req, fp, code, msg, headers):
     return headers.getheaders('location')[0]
   http_error_301 = http_error_302 = http_error_307 = http_error_303
 
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-openerNo303Handler = urllib2.build_opener(MyHTTPRedirectHandler, urllib2.HTTPCookieProcessor(cj))
+class MoodlefetchGetFilenames(threading.Thread):    
+    def __init__(self, parent, course):
+        threading.Thread.__init__(self)
+        self.parent = parent
+        self.course = course
+    def run(self):
+        uri = self.parent.baseuri+'/course/view.php?id='+self.course.id
+        req = urllib2.Request(uri)
+        f = self.parent.opener.open(req)
+        data = f.read()
+        matches = re.findall(r'(?<=href=").*pdf\.gif" class="activityicon" alt="" \/> <span>.*<span', data)
+        for match in matches:
+            file = File()
+            file.id = re.findall(r'(?<=id=)[0-9]+', match)[0]
+            file.type = 'pdf' #TODO / just for now
+            uri = self.parent.baseuri+'/mod/resource/view.php?inpopup=true&id='+file.id
+            uri = self.parent.openerNo303Handler.open(uri)
+            srcurl, file.name = uri.rsplit('/', 1)
+            self.course.addFileAvailable(file)
+            logger.debug("course.files_available: added file "+file.name+" with id: "+str(file.id))
 
-def usage():
-  print('usage: %s [configfile]' % sys.argv[0])
+class MoodlefetchDownloadFile(threading.Thread):    
+    def __init__(self, parent, course, file):
+        threading.Thread.__init__(self)
+        self.parent = parent
+        self.course = course
+        self.file = file
+    def run(self):
+        if not os.path.exists(self.course.path):
+            try:
+                os.makedirs(self.course.path)
+                logger.info('created directory '+self.course.path)
+            except:
+                logger.error('problem creating directory '+self.course.path)
+        uri = self.parent.baseuri+'/mod/resource/view.php?inpopup=true&id='+str(self.file.id)
+        req = urllib2.Request(uri)
+        f = self.parent.opener.open(req)
+        try:
+            localFile = open(self.course.path+self.file.name, 'w')
+            localFile.write(f.read())
+            localFile.close()
+            logger.info('file saved: '+self.course.path+self.file.name)
+        except:
+            logger.error("failed to write "+self.course.path+self.file.name)
 
-config = ConfigParser.RawConfigParser()
-if(len(sys.argv) == 2):
-  if os.path.isfile(sys.argv[1]):
-    config.read(sys.argv[1])
-  else:
-    if os.path.isfile(os.path.expanduser(DEFAULTCONFIG)):
-      config.read(os.path.expanduser(DEFAULTCONFIG))
-    else:
-      usage()
-      sys.exit()
-else:
-  if os.path.isfile(os.path.expanduser(DEFAULTCONFIG)):
-    config.read(os.path.expanduser(DEFAULTCONFIG))
-  else:
-    usage()
-    sys.exit()
-
-if config.get('general', 'output') == "notify":
-  import pynotify
-if config.get('general', 'output') == "growl":
-  import gntp.notifier
-if config.get('general', 'keyring') == "true":
-  import keyring
-
-def moodle_login(username, password):
-  uri = 'https://elearning.fh-hagenberg.at/login/index.php'
-  req = urllib2.Request(uri)
-  f = opener.open(req)
-  data = f.read()
-  formFields = (
+class Moodlefetch():
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    openerNo303Handler = urllib2.build_opener(MyHTTPRedirectHandler, urllib2.HTTPCookieProcessor(cj))
+    semesterid = 0
+    courses = []
+    local_files = []
+    baseuri = 'https://elearning.fh-hagenberg.at'
+    dir = "/tmp/moodlefetch/"
+    def login(self, username, password):
+        uri = self.baseuri+'/login/index.php'
+        req = urllib2.Request(uri)
+        f = self.opener.open(req)
+        data = f.read()
+        formFields = (
               (r'username', username),
-              (r'password', password)
+              (r'password', password),
               )
-  encodedFields = urllib.urlencode(formFields)
-  req = urllib2.Request(uri, encodedFields)
-  f = opener.open(req)
-  if f:
-    debug('login successful')
+        encodedFields = urllib.urlencode(formFields)
+        req = urllib2.Request(uri, encodedFields)
+        self.opener.open(req)
+        #TODO checking on cj doesnt help anything
+        if self.cj:
+            logger.info("logged in")
+        else:
+            logger.error("login failed. exiting")
+    def getSemesterId(self, semester):
+        uri = self.baseuri+'/?role=0&cat=1&stg=all&sem=&csem=0'
+        req = urllib2.Request(uri)
+        f = self.opener.open(req)
+        response = f.read()
+        response = re.findall(r'(?<=\<option\ value=").*>'+semester+'<\/option>', response)
+        self.semesterid = re.split('"', response[0])[0]
+    def getCourses(self):
+        uri = self.baseuri+'/?role=0&cat=1&stg=all&sem='+self.semesterid+'&csem=0'
+        req = urllib2.Request(uri)
+        f = self.opener.open(req)
+        data = f.read()
+        matches = re.findall(r'(?<=course\/view\.php\?id\=).*</a>', data)
+        for match in matches:
+            course = Course()
+            split = re.split('\.', re.sub(', ', '.', re.sub('">', '.', re.sub('</a>', '', match))))
+            course.id = split[0]
+            course.name = split[3]+"-"+split[5]
+            try:
+                course.name = config.get('courses', Course.name)
+            except:
+                course.name = split[3]+"-"+split[5]
+            course.path = self.dir+course.name+'/'
+            self.courses.append(course)
+    def getLocalFiles(self):
+        for top, dirs, files in os.walk(self.dir):
+            for nm in files:
+                self.local_files.append(os.path.join(top, nm))
+    def sync(self):
+        self.getLocalFiles()
+        for course in self.courses:
+            for file in course.files_available:
+                if course.path+file.name not in self.local_files:
+                    course.addFileToGet(file)
+                    logger.debug("course.files_to_get: added file "+file.name+" with id: "+str(file.id))
+            for file in course.files_to_get:
+                while True: #we only want a maximum of 5 parallel downloads
+                    if(threading.activeCount() < 6):
+                        thread = MoodlefetchDownloadFile(self, course, file)
+                        thread.start()
+                        logger.debug('started download thread for file: '+file.name)
+                        break
+                    else:
+                        time.sleep(0.1)
+        while (threading.activeCount() > 1):
+            pass
+        
+    def __init__(self, username, password):
+        self.login(username, password)
+        self.getSemesterId('SS12')
+        self.getCourses()
+        for course in self.courses:
+            thread = MoodlefetchGetFilenames(self, course)
+            thread.start()
+        while (threading.activeCount() > 1):
+            pass
+        self.sync()
+            
+class Course:
+    def __init__(self):
+        self.id = 0
+        self.name = ""
+        self.path = ""
+        self.files_available = []
+        self.files_to_get = []
+    def addFileAvailable(self, file):
+        self.files_available.append(file)
+    def addFileToGet(self, file):
+        self.files_to_get.append(file)
 
-def moodle_deadlines():
-  uri = 'http://elearning.fh-hagenberg.at/calendar/view.php?view=upcoming'
-  req = urllib2.Request(uri)
-  f = opener.open(req)
-  data = f.read()
-  ids = re.findall(r'(?<=\/mod\/assignment\/view\.php\?id\=)[^"]+', data)
-  for assignmentId in ids:
-    uri = 'http://elearning.fh-hagenberg.at/mod/assignment/view.php?id='+assignmentId
-    req = urllib2.Request(uri)
-    f = opener.open(req)
-    data = f.read()
-    date = re.findall(r'(?<=Abgabetermin:<\/td>    <td class="c1">)[^<]+', data)
-    title = re.findall(r'(?<=<title>)[^<]+', data)
-    print title[0]
-    print "  Deadline: "+date[0]
-    print ""
+class File:
+    def __init__(self):
+        self.id = 0
+        self.type = "" #for future use
+        self.name = ""
 
-def moodle_ratings(semester):
-  uri = 'https://elearning.fh-hagenberg.at/?role=0&cat=1&stg=all&sem=&csem=0'
-  req = urllib2.Request(uri)
-  f = opener.open(req)
-  data = f.read()
-  data = re.findall(r'(?<=\<option\ value=").*>'+semester+'<\/option>', data)
-  semesterid = re.split('"', data[0])
-  uri = 'https://elearning.fh-hagenberg.at/?role=0&cat=1&stg=all&sem='+semesterid[0]+'&csem=0'
-  req = urllib2.Request(uri)
-  f = opener.open(req)
-  data = f.read()
-  matches = re.findall(r'(?<=course\/view\.php\?id\=).*</a>', data)
-  for match in matches:
-    split = re.split('\.', re.sub(', ', '.', re.sub('">', '.', re.sub('</a>', '', match))))
-    courseid = split[0]
-    coursename = split[3]+"-"+split[5]
-    print coursename
-    uri = 'http://elearning.fh-hagenberg.at/grade/report/user/index.php?id='+courseid
-    req = urllib2.Request(uri)
-    f = opener.open(req)
-    data = f.read()
-    gradeIds = re.findall(r'(?<=grade\.php\?id\=)[^"]+', data)
-    for gradeid in gradeIds:
-      uri = 'http://elearning.fh-hagenberg.at/mod/assignment/view.php?id='+gradeid
-      req = urllib2.Request(uri)
-      f = opener.open(req)
-      data = f.read()
-      rating = re.findall(r'(?<=class\="grade">)[^<]+', data)
-      title = re.findall(r'(?<=<title>)[^<]+', data)
-      try:
-        print "  - "+title[0]
-      except:
-        print "  - untitled"
-      try:
-        print "  - RATING: "+rating[0]
-      except:
-        print "  - RATING: -"
-      
-  
-
-def moodle_getcourses(semester):
-  uri = 'https://elearning.fh-hagenberg.at/?role=0&cat=1&stg=all&sem=&csem=0'
-  req = urllib2.Request(uri)
-  f = opener.open(req)
-  data = f.read()
-  data = re.findall(r'(?<=\<option\ value=").*>'+semester+'<\/option>', data)
-  semesterid = re.split('"', data[0])
-  uri = 'https://elearning.fh-hagenberg.at/?role=0&cat=1&stg=all&sem='+semesterid[0]+'&csem=0'
-  req = urllib2.Request(uri)
-  f = opener.open(req)
-  data = f.read()
-  matches = re.findall(r'(?<=course\/view\.php\?id\=).*</a>', data)
-  for match in matches:
-    split = re.split('\.', re.sub(', ', '.', re.sub('">', '.', re.sub('</a>', '', match))))
-    courseid = split[0]
-    coursename = split[3]+"-"+split[5]
-    try:
-      coursename = config.get('courses', coursename)
-    except:
-      coursename = split[3]+"-"+split[5]
-    debug("looking for new files in: "+coursename)
-    moodle_getfiles(courseid, coursename, config.get('general', 'dstdir'))
-
-def moodle_getfiles(courseid, coursename, dstdir):
-  path = dstdir+'/'+coursename
-  if not os.path.exists(path):
-    debug('creating directory '+path)
-    os.makedirs(path)
-  localFiles = getLocalFiles(path)
-  uri = 'https://elearning.fh-hagenberg.at/course/view.php?id='+courseid
-  req = urllib2.Request(uri)
-  f = opener.open(req)
-  data = f.read()
-  matches = re.findall(r'(?<=href=").*pdf\.gif" class="activityicon" alt="" \/> <span>.*<span', data)
-  for match in matches:
-    fileid = re.findall(r'(?<=id=)[0-9]+', match)
-    files = re.findall(r'(?<=<span>).*<', match)
-    # filename['htmlsrc'] and filename['orig'] added to provide compatibility with archives generated before 2012-03-30
-    filename = {'htmlsrc':  '',
-                'orig':     ''}
-    filename['htmlsrc'] = path+'/'+re.sub('/', '_', re.sub('\<', '.pdf', re.sub(' ', '_', files[0])))
-    uri = 'https://elearning.fh-hagenberg.at/mod/resource/view.php?inpopup=true&id='+fileid[0]
-    uri = openerNo303Handler.open(uri)
-    srcurl, filename['orig'] = uri.rsplit('/', 1)
-    try:
-      filename['orig'], args = filename['orig'].rsplit('?', 1)
-    except:
-      filename['orig'] = filename['orig']
-    
-    filename['orig'] = path+"/"+filename['orig']
-    #take care of older archives
-    if filename['htmlsrc'] in localFiles:
-      if filename['htmlsrc'] != filename['orig']:
-        debug("moved "+filename['htmlsrc']+" "+path+"/"+filename['orig'])
-        os.rename(filename['htmlsrc'], filename['orig'])
-    if not config.get('general', 'forcedownload') == "true":
-      if filename['orig'] not in localFiles:
-        #also for older archives...
-        if filename['htmlsrc'] not in localFiles:
-          moodle_fedch(uri, filename['orig'])
-    else:
-        moodle_fedch(uri, filename['orig'])
-
-def moodle_fedch(uri, filename):
-  req = urllib2.Request(uri)
-  f = opener.open(req)
-  debug('writing: '+filename)
-  localFile = open(filename, 'w')
-  localFile.write(f.read())
-  localFile.close()
-  
-
-def getLocalFiles(dstdir):
-  localFiles = list()
-  for top, dirs, files in os.walk(dstdir):
-    for nm in files:       
-      localFiles.append(os.path.join(top, nm))
-  return localFiles
-
-def moodle_logout():
-  uri = "https://elearning.fh-hagenberg.at/index.php"
-  req = urllib2.Request(uri)
-  f = opener.open(req)
-  data = f.read()
-  data = re.findall(r'(?<=logout\.php\?sesskey=)[^"]+', data)
-  sessionkey = data[0]
-  uri = 'http://elearning.fh-hagenberg.at/login/logout.php?sesskey='+sessionkey
-  req = urllib2.Request(uri)
-  f = opener.open(req)
-  data = f.read()
-  if re.search('Sie sind nicht angemeldet', data):
-    debug('logout successful')
-  else:
-    debug('logout failed')
-
-def debug(msg):
-  if config.get('general', 'output') == "stdout":
-    print msg
-  if config.get('general', 'output') == "notify":
-    n = pynotify.Notification("moodlefetch", msg)
-    n.show()
-  if config.get('general', 'output') == "growl":
-    gntp.notifier.mini(msg)
-
-if config.get('general', 'output') == "notify":
-  pynotify.init("moodlefedch")
-
-if config.get('general', 'keyring') == "true":
-  try:
-    password = keyring.get_password("moodlefetch", config.get('moodle', 'username'))
-    if password == None:
-      keyring.set_password("moodlefetch", config.get('moodle', 'username'), getpass.getpass())
-      password = keyring.get_password("moodlefetch", config.get('moodle', 'username'))
-  except:
-    debug("getting password from keyring failed...")
-    sys.exit(1)
-else:
-  if not config.get('moodle', 'password'):
-    password = getpass.getpass()
-  else:
-    password = config.get('moodle', 'password')
-
-if(len(sys.argv) == 2):
-  if(sys.argv[1] == "--deadlines"):
-    moodle_login(config.get('moodle', 'username'), password)
-    moodle_deadlines()
-    moodle_logout()
-  elif(sys.argv[1] == "--ratings"):
-    moodle_login(config.get('moodle', 'username'), password)
-    moodle_ratings(config.get('moodle', 'semester'))
-    moodle_logout()
-else:
-  moodle_login(config.get('moodle', 'username'), password)
-  moodle_getcourses(config.get('moodle', 'semester'))
-  moodle_logout()
+#no config parsing yet
+username = "S1110239019"
+password=""
+moodle = Moodlefetch(username, password)
